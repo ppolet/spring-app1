43c848365a379afb0b58421f3e2a497afa0496bc
pm.poletaev <ppolet@mail.ru>
2021-07-30 23:37:01 +0300

    lesson 10 - @Autowired bean ClassicalMusic

--- src/main/java/org/example/springcourseAnnotation/MusicPlayer.java
+++ src/main/java/org/example/springcourseAnnotation/MusicPlayer.java
@@ -1,40 +1,20 @@
 package org.example.springcourseAnnotation;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MusicPlayer {
-    private Music music;
+    private ClassicalMusic classicalMusic;
 
-    private String name;
-    private int volume;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getVolume() {
-        return volume;
-    }
-
-    public void setVolume(int volume) {
-        this.volume = volume;
-    }
-
-    public MusicPlayer() {}
-
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
-
-    public void setMusic(Music music) {
-        this.music = music;
+    @Autowired
+     public MusicPlayer(ClassicalMusic classicalMusic) {
+        this.classicalMusic = classicalMusic;
     }
 
     //---проигрывание 1 песни
     public void playMusic(){
-        System.out.println("Playing: " + music.getSong());
+        System.out.println("Playing: " + classicalMusic.getSong());
     }
 
 }
--- src/main/java/org/example/springcourseAnnotation/TestSpring.java
+++ src/main/java/org/example/springcourseAnnotation/TestSpring.java
@@ -6,14 +6,17 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContextAnnotation.xml");
 
-        Music music = context.getBean("rockMusic", Music.class);
-        MusicPlayer musicPlayer = new MusicPlayer(music);
+//        Music music = context.getBean("rockMusic", Music.class);
+//        MusicPlayer musicPlayer = new MusicPlayer(music);
+//        musicPlayer.playMusic();
+//
+//        Music music2 = context.getBean("classicalMusic", Music.class);
+//        MusicPlayer classicalMusicPlayer = new MusicPlayer(music2);
+//        classicalMusicPlayer.playMusic();
+
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
-
-        Music music2 = context.getBean("classicalMusic", Music.class);
-        MusicPlayer classicalMusicPlayer = new MusicPlayer(music2);
-        classicalMusicPlayer.playMusic();
-
+        
         context.close();
     }
 }
