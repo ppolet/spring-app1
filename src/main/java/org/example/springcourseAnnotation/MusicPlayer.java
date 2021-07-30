27678103f3d44bcc27e709a2b8f237f16abf2950
pm.poletaev <ppolet@mail.ru>
2021-07-30 23:47:06 +0300

    lesson 10 - @Autowired - only bean RockMusic - private Music music

--- src/main/java/org/example/springcourseAnnotation/MusicPlayer.java
+++ src/main/java/org/example/springcourseAnnotation/MusicPlayer.java
@@ -5,16 +5,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
-    private Music music;
-
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
-
     @Autowired
-    public void setMusic(Music music) {
-        this.music = music;
-    }
+    private Music music;
 
     //---проигрывание 1 песни
     public void playMusic(){
