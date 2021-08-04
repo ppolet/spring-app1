e02787f8656b0edbeccd463479460b3215413587
pm.poletaev <ppolet@mail.ru>
2021-08-04 22:32:27 +0300

    lesson 13 - SpringConfig.java - Домашнее задание в новом springcourceAnnotationJava package

---
+++ src/main/java/org/example/springcourseAnnotationJava/JazzMusic.java
@@ -0,0 +1,8 @@
+package org.example.springcourseAnnotationJava;
+
+public class JazzMusic implements Music{
+    @Override
+    public String getSong() {
+        return "Java JAZZ music";
+    }
+}
